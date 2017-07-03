3c0ab8d09698adc9d5ea18b18e790384b136cf0c
Fernando <fernando.sversiani@gmail.com>
2017-07-03 18:07:05 -0300

    Adicionar arquivos de projeto.

---
+++ ControVTCard.Web/Models/UsuarioModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ControVTCard.Web.Models
+{
+    public class UsuarioModel
+    {
+        public static bool ValidarUsuario(String login, string senha)
+        {
+            var ret = false;
+            using (var conexao = new SqlConnection())
+            {
+                conexao.ConnectionString = ConfigurationManager.ConnectionStrings["principal"].ConnectionString;
+                conexao.Open();
+                using (var comando = new SqlCommand())
+                {
+                    comando.Connection = conexao;
+                    comando.CommandText = string.Format(
+                        "select count(*)from usuario where login='{0}' and senha='{1}'", login, senha);
+                    ret = ((int)comando.ExecuteScalar() > 0);
+                }
+            }
+            return ret;
+        }
+    }
+}
